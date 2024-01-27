9be2fd9d37c2ccdede11c26657539aa3013fdc42
kuanghc <609779612@qq.com>
2024-01-27 22:13:38 +0800

    move record

--- src/main/java/com/example/demo/exception/ApiException.java
+++ src/main/java/com/example/demo/exception/ApiException.java
@@ -1,13 +1,17 @@
 package com.example.demo.exception;
 
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
 // private final, getter, public constructor, equals, hashcode
 // and to string
-public record ApiException (String message,
-                            Throwable throwable,
-                            HttpStatus httpStatus,
-                            ZonedDateTime zonedDateTime) {
+@Data
+public class ApiException {
+
+    private final String message;
+    private final Throwable throwable;
+    private final HttpStatus httpStatus;
+    private final ZonedDateTime timestamp;
 }
